90ed3a68bbb968bb51acf0c0f6098d1c757edf97
Khali Hill <khali.hill@gmail.com>
2022-05-09 17:32:00 -0500

    created models and migrations for Builds and Themes

---
+++ commands.md
@@ -0,0 +1,3 @@
+npx sequelize model:generate --name Build --attributes name:string,imageLink:text,legoItemNumber:integer,pieceCount:integer
+
+npx sequelize model:generate --name Theme --attributes name:string
---
+++ db/migrations/20220509222001-create-build.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Builds', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(75)
+      },
+      imageLink: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      legoItemNumber: {
+        unique: true,
+        type: Sequelize.INTEGER
+      },
+      pieceCount: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Builds');
+  }
+};
