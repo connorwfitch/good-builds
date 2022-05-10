8559cd9bf9100f5b4948bc2a95311b4b7038213d
connorwfitch <connorwfitch@gmail.com>
2022-05-10 10:50:08 -0400

    continuing work on signup functionality, stopping before class

--- routes/signup.js
+++ routes/signup.js
@@ -3,22 +3,64 @@ const express = require('express');
 const { check, validationResult } = require('express-validator');
 
 // internal modules
-const { csrfProtection, asyncHandler } = require('./utils');
+const { csrfProtection, asyncHandler, bcrypt } = require('./utils');
 const { loginUser, logoutUser } = require('../auth');
 
 // creating router
 const router = express.Router();
 
+/*
+--------------VALIDATORS--------------
+*/
+// TODO: add sign up validations
 
-/* GET sign up page. */
-router.get('/', (req, res) => {
+const userValidators = [];
+
+/*
+--------------ROUTES--------------
+*/
+
+// GET sign up page.
+router.get('/', csrfProtection, (req, res) => {
     const user = db.User.build();
-    res.render('sign-up-form');
+    res.render('sign-up', {
+        user,
+        csrfToken: req.csrfToken(),
+    });
 });
 
 /* POST sign up page. */
-router.post('/signup', asyncHandler(async(req, res) => {
-    // 
+router.post('/signup', csrfProtection, userValidators, asyncHandler(async(req, res) => {
+    const {
+        firstName,
+        lastName,
+        emailAddress,
+        password
+    } = req.body;
+
+    const user = db.User.build({
+        firstName,
+        lastName,
+        emailAddress
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+        const hashedPassword = await bcrypt.hash(password, 12)
+        user.hashedPassword = hashedPassword;
+        await user.save();
+        loginUser(req, res, user);
+        // TODO: decide where we want to redirect users after successful login
+        res.redirect('/');
+    } else {
+        const errors = validatorErrors.array().map((error) => error.msg);
+        res.render('sign-up', {
+            user,
+            errors,
+            csrfToken: req.csrfToken(),
+        });
+    }
 }));
 
 module.exports = router;
