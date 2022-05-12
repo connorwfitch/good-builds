67021b727c2c771323bff0afe49af78fdb62e2ee
connorwfitch <connorwfitch@gmail.com>
2022-05-12 16:11:56 -0400

    minor changes to display shelves routes, added pug template for the form

--- routes/displayshelves.js
+++ routes/displayshelves.js
@@ -15,82 +15,64 @@ const router = express.Router();
 */
 
 const displayShelfValidators = [
-    check('title')
-        .exists({ checkFalsy: true })
-        .withMessage('Please provide a Title')
-        .isLength({ max: 75 })
-        .withMessage('Title must not be more than 75 characters long'),
-    check('subtitle')
-        .exists({ checkFalsy: true })
-        .withMessage('Please provide a Subtitle')
-        .isLength({ max: 75 })
-        .withMessage('Subtitle must not be more than 75 characters long'),
+  check('title')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a Title')
+    .isLength({ max: 75 })
+    .withMessage('Title must not be more than 75 characters long'),
+  check('subtitle')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a Subtitle')
+    .isLength({ max: 150 })
+    .withMessage('Subtitle must not be more than 150 characters long'),
 ];
 
 /*
 --------------ROUTES--------------
 */
 
-// // GET Display Shelf on browse page
-// router.get('/', asyncHandler(async (req, res) => {
-//   const displayShelves = await db.DisplayShelf.findAll();
-
-//   res.render('displayShelves-browse', { 
-//     title: 'Display Shelves',
-//     displayShelves,
-//   });
-// }));
-
 // GET new display shelf page.
 router.get('/new', requireAuth, csrfProtection, (req, res) => {
-    const displayShelf = db.DisplayShelf.build();
+  const displayShelf = db.DisplayShelf.build();
 
-    res.render('new-displayShelf', {
-      title: 'Create New Display Shelf',
-      displayShelf,
-      csrfToken: req.csrfToken(),
-    });
+  res.render('new-display-shelf', {
+    title: 'Create New Display Shelf',
+    displayShelf,
+    csrfToken: req.csrfToken(),
   });
+});
 
 
 
 // POST new display shelf page.
-router.post('/'/* does this need to be "/new"? */, requireAuth, csrfProtection, displayShelfValidators, asyncHandler(async (req, res) => {
-    const {
-      title,
-      subtitle
-    } = req.body;
-  
-    const { userId } = req.session.auth;
-  
-    const displayShelf = db.DisplayShelf.build({
-      title,
-      subtitle,
-      userId
+router.post('/', requireAuth, csrfProtection, displayShelfValidators, asyncHandler(async (req, res) => {
+  const {
+    title,
+    subtitle
+  } = req.body;
+
+  const { userId } = req.session.auth;
+
+  const displayShelf = db.DisplayShelf.build({
+    title,
+    subtitle,
+    userId
+  });
+
+  const validatorErrors = validationResult(req);
+
+  if (validatorErrors.isEmpty()) {
+    await displayShelf.save();
+    req.session.save(() => res.redirect('/builds'))
+  } else {
+    const errors = validatorErrors.array().map((error) => error.msg);
+    res.render('new-display-shelf', {
+      title: 'Create New Display Shelf',
+      displayShelf,
+      errors,
+      csrfToken: req.csrfToken(),
     });
+  }
+}));
   
-    if (title) {
-      displayShelf.title = title;
-    }
-  
-    if (subtitle) {
-      build.subtitle = subtitle;
-    }
-  
-    const validatorErrors = validationResult(req);
-  
-    if (validatorErrors.isEmpty()) {
-      await displayShelf.save();
-      req.session.save(() => res.redirect(`displayShelves/${displayShelf.id}`))
-    } else {
-      const errors = validatorErrors.array().map((error) => error.msg);
-      res.render('new-displayShelf', {
-        title: 'Create New Display Shelf',
-        displayShelf,
-        errors,
-        csrfToken: req.csrfToken(),
-      });
-    }
-  }));
-  
-  module.exports = router;
+module.exports = router;
