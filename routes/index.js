84d291b6ac6a7822c15a144996a416888bdcc0fe
connorwfitch <connorwfitch@gmail.com>
2022-05-13 15:36:12 -0400

    small progress on the splash page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
+// external modules
 const express = require('express');
+
+// internal modules
+const db = require('../db/models');
+const { asyncHandler } = require('./utils');
+
+// creating the router
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', asyncHandler( async function(req, res, next) {
+
+  const newestBuilds = await db.Build.findAll({
+    limit: 8,
+    order: [['createdAt', 'DESC']],
+  })
 
   res.render('index', { 
     title: 'GoodBuilds',
+    newestBuilds
   });
-});
+}));
 
 module.exports = router;
